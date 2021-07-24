14907988d0b81515b64b69a0463005de2900d1b9
Ayden Khalil <akha0058@student.monash.edu>
2021-07-25 05:28:02 +1000

    first night grind

--- src/App.js
+++ src/App.js
@@ -1,47 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
+import BasicTable from './Table';
+import DataGridDemo from './Grid'
+
 
 export default function App() {
   return (
     <React.Fragment>
       <Header>
-        <TextHeader>
-          <h1>A.R</h1>
-        </TextHeader>
-      </Header>  
+        <TextHeader><h1>Rishika's Calculator ❤</h1></TextHeader>
+      </Header>
+      <Viewport>
+        <DataGridDemo/>
+      </Viewport>
+      
     </React.Fragment>
     );
 }
 
 
-const Header = styled.div`
-  background-color: #0071B3;
+const Viewport = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: space-evenly;
+`
 
+const Header = styled.div`
+  background-color: #d12e61;
+  width: 100%;
+  height: 10vh;
+  display: flex;
+  flex-direction: row;
 `
 
 const TextHeader = styled.div`
   color: white;
-  position: relative;
-  width: 70%; 
-  
+  font-family: "Helvetica Neue",Arial,sans-serif;
   display: flex;
   flex-direction: row;
-  
-  font-family: "Helvetica Neue",Arial,sans-serif;
   font-weight: 400;
-  font-style: normal;
   font-size: 15px;
-  letter-spacing: 0em;
   line-height: 1.8em;
+
   h1 {
-    margin: 0 .5em;
-    padding: .618em 0;
-    cursor: pointer;
+    padding: 0;
+    margin-left: 2vw;
+    
   }
-  h1:hover {
-    color: #68c0e8;
-  }
-  
 `
